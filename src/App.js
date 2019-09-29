2dcd8315d758302d84ea493cb51fcf7f05be12d3
Swizec Teller <swizec@swizec.com>
2019-09-29 15:17:51 -0700

    Drive teh main Year animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import * as d3 from "d3";
 import faker from "faker";
@@ -11,29 +11,64 @@ const Svg = styled.svg`
     background: #0b0c10;
 `;
 
-function App() {
-    // Create 5 imaginary processors
-    const processors = d3.range(5).map(_ => faker.hacker.noun()),
-        random = d3.randomUniform(1000, 50000);
+const useData = () => {
+    const [data, setData] = useState(null);
 
-    // create random transistor counts for each year
-    const data = d3.range(1970, 2025).map(year =>
-        d3.range(5).map(i => ({
-            year: year,
-            name: processors[i],
-            transistors: Math.round(random())
-        }))
-    );
+    // Replace this with actual data loading
+    useEffect(() => {
+        // Create 5 imaginary processors
+        const processors = d3.range(5).map(_ => faker.hacker.noun()),
+            random = d3.randomUniform(1000, 50000);
+
+        // create random transistor counts for each year
+        const data = d3.range(1970, 2025).map(year =>
+            d3.range(5).map(i => ({
+                year: year,
+                name: processors[i],
+                transistors: Math.round(random())
+            }))
+        );
+
+        setData(data);
+    }, []);
+
+    return data;
+};
+
+function App() {
+    const data = useData();
+    const [currentYear, setCurrentYear] = useState(1970);
+
+    const yearIndex = d3
+        .scaleLinear()
+        .domain([1970, 2025])
+        .range([0, 2025 - 1970]);
+
+    // Drives the main animation progressing through the years
+    // It's actually a simple counter :P
+    useEffect(() => {
+        const interval = d3.interval(() => {
+            if (currentYear >= 2024) {
+                interval.stop();
+            }
+
+            setCurrentYear(year => year + 1);
+        }, 3000);
+
+        return () => interval.stop();
+    }, []);
 
     return (
         <Svg>
-            <Barchart
-                data={data[0]}
-                x={100}
-                y={50}
-                barThickness={20}
-                width={500}
-            />
+            {data ? (
+                <Barchart
+                    data={data[yearIndex(currentYear)]}
+                    x={100}
+                    y={50}
+                    barThickness={20}
+                    width={500}
+                />
+            ) : null}
         </Svg>
     );
 }
