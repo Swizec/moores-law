c04f920521076d8ab819fc2c40ab9bf6afc9b77e
Swizec Teller <swizec@swizec.com>
2019-09-29 15:00:51 -0700

    Add labels to bars

--- src/Barchart.js
+++ src/Barchart.js
@@ -1,8 +1,24 @@
 import React from "react";
 import * as d3 from "d3";
+import styled from "styled-components";
+
+const Label = styled.text`
+    fill: white;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+        "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+        "Helvetica Neue", sans-serif;
+    font-size: 14px;
+    text-anchor: end;
+    alignment-baseline: middle;
+`;
 
 const Bar = ({ data, y, width, thickness }) => {
-    return <rect x={0} y={y} width={width} height={thickness} fill="white" />;
+    return (
+        <g transform={`translate(${0}, ${y})`}>
+            <rect x={10} y={0} width={width} height={thickness} fill="white" />
+            <Label y={thickness / 2}>{data.name}</Label>
+        </g>
+    );
 };
 
 // Draws the barchart for a single year
